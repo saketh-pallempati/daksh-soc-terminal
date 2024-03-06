601545c39a4208d4110f6ee56e8900b1f3dd59d6
Saketh Pallempati <90235687+SAKETH-PALLEMPATI@users.noreply.github.com>
2024-03-06 19:28:07 +0530

    first commit

--- src/utils/bin/api_commands.ts
+++ src/utils/bin/api_commands.ts
@@ -1,36 +1,74 @@
 // // List of commands that require API calls
 
-import { getProjects } from '../api';
-import { getQuote } from '../api';
-import { getReadme } from '../api';
-import { getWeather } from '../api';
+import axios from 'axios';
+axios.defaults.withCredentials = true;
 
-export const projects = async (args: string[]): Promise<string> => {
-  const projects = await getProjects();
-  return projects
-    .map(
-      (repo) =>
-        `${repo.name} - <a class="text-light-blue dark:text-dark-blue underline" href="${repo.html_url}" target="_blank">${repo.html_url}</a>`,
-    )
-    .join('\n');
-};
-
-export const quote = async (args: string[]): Promise<string> => {
-  const data = await getQuote();
-  return data.quote;
-};
-
-export const readme = async (args: string[]): Promise<string> => {
-  const readme = await getReadme();
-  return `Opening GitHub README...\n
-  ${readme}`;
-};
-
-export const weather = async (args: string[]): Promise<string> => {
-  const city = args.join('+');
-  if (!city) {
-    return 'Usage: weather [city]. Example: weather casablanca';
+export const whoami = async (args: string[]): Promise<string> => {
+  const response = await axios.get('http://localhost:3000/verify', {
+    withCredentials: true,
+  });
+  if (response.data.user) {
+    return response.data.user.username;
   }
-  const weather = await getWeather(city);
-  return weather;
+  return `Login to find out 😉`;
+};
+
+export const message = async (args: string[]): Promise<string> => {
+  const msg = args.join(' ');
+  if (!msg) {
+    return 'Usage: message [message]. Example: message "Need help with something!"';
+  }
+  const response = await axios.post(
+    'http://localhost:3000/game/message',
+    {
+      message: msg,
+    },
+    { withCredentials: true },
+  );
+  return response.data.message;
+};
+
+export const submit = async (args: string[]): Promise<string> => {
+  const comment = args.join(' ');
+  if (!comment) {
+    return 'Usage: submit [Vulnerability]. Example: submit unfunctional button at the left corner of the page';
+  }
+  const response = await axios.post(
+    'http://localhost:3000/game/check',
+    {
+      comment: comment,
+    },
+    { withCredentials: true },
+  );
+  if (response.data.flag === true) {
+    return 'Vulnerability submitted successfully';
+  } else {
+    return 'Keep looking for vulnerabilities';
+  }
+};
+
+export const login = async (args: string[]): Promise<string> => {
+  const email = args[0];
+  const password = args[1];
+  if (!email || !password) {
+    return 'Usage: login [email] [password]';
+  }
+  try {
+    const response = await axios.post(
+      'http://localhost:3000/login',
+      {
+        email: email,
+        password: password,
+      },
+      { withCredentials: true },
+    );
+
+    if (response.data.status === true) {
+      return 'Logged in successfully';
+    } else {
+      return response.data.message;
+    }
+  } catch (error) {
+    return error.toString();
+  }
 };
